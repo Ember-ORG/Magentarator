c7bf5796b813835afedb6b2e4078f806e46beec4
Execodeable <IProgramEmail@gmail.com>
2017-11-11 11:59:57 -0700

    Add files via upload

---
+++ script.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"honnef.co/go/js/dom"
+)
+
+func main() {
+	d := dom.GetWindow().Document()
+	foo := d.GetElementByID("create").(*dom.HTMLButtonElement)
+	foo.AddEventListener("click", false, func(event dom.Event) {
+		println("Click")
+	})
+}
