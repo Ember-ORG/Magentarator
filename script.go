aa947f79ac656e137dafc3072a310de560b02d8b
MrLittleman <iantdraves@gmail.com>
2017-11-11 12:05:30 -0700

    Test

--- script.go
+++ script.go
@@ -8,6 +8,6 @@ func main() {
 	d := dom.GetWindow().Document()
 	foo := d.GetElementByID("create").(*dom.HTMLButtonElement)
 	foo.AddEventListener("click", false, func(event dom.Event) {
-		println("Click")
+		println("Test")
 	})
 }
